2aa0e0a1ba2b5eb99c88853680ee8661edd10cca
Jelle0Bol <99067766@mydavinci.nl>
2021-10-29 13:10:42 +0200

    bon

--- ijs2.py
+++ ijs2.py
@@ -3,6 +3,7 @@ import sys
 
 ijssalon = 1
 bak = 1
+hoorn = 1.25
 def smaken():
     global smaak
     for k in range(bol ,0,-1):
@@ -23,37 +24,62 @@ def bestel():
     global bestel
     bestellen = input("Hier is uw "+ deel+ " met " + str(bol)+ " bolletje(s). Wilt u nog meer bestellen? (Y/N) : ").lower()
     if bestellen == "y":
-        ijssalon+0
+        return begin()
     elif bestellen == "n":
+        bonnetje()
         print("Bedankt en tot ziens.")
-        sys.exit()
-        
+        sys.exit()     
     else:
         print("Sorry, dat snap ik niet...")
         ijssalon+0
 
-print("Welkom bij Papi Gelato. ")
-while ijssalon == 1:
-    bol = int(input("Hoeveel bolletjes ijs wilt u? : "))
-    while bol >8:
-        print("Sorry, zulke grote bakken hebben we niet")
+def bonnetje():
+    global bon
+    #prijsBak = (bakjeAantal * 0.75)
+    #prijsHoorn = (hoorntjeAantal * 1.25)
+    print("")
+    print("-------------[Papi Gelato]-------------")
+    print("")
+    #print("Bolletjes   ",bol," x €1,10 = €",prijsBol)
+    total = "{:.2f}".format(round(float(bol)*1.1 , 2))
+    totalBakje = "{:.2f}".format(round(float(bakjeAantal)*0.75 , 2))
+    totalHoorntje = "{:.2f}".format(round(float(hoorntjeAantal)*1.25 , 2))
+    totaalBon = total + totalBakje + totalHoorntje
+    print("Bolletjes:       ", bol ,           "x € 1.10 =  €",total)
+    print("Horrentje:       ", hoorntjeAantal ,"x € 1.25 =  €",totalHoorntje)
+    print("Bakje:           ", bakjeAantal,    "x € 0.75 =  €",totalBakje)
+    print("                                ------ +")
+    print("Totaal:                         €",totaalBon)
+
+def begin():
+    global bol, deel, hoorntjeAantal, bakjeAantal
+    hoorntjeAantal = 0
+    bakjeAantal = 0
+    while ijssalon == 1:
         bol = int(input("Hoeveel bolletjes ijs wilt u? : "))
-    if bol >=1 and bol <=3:
-        while bak == 1:
-            hoorntje = str(input("Wilt u deze " + str(bol) + " bolletje(s) in A) een hoorntje of B) een bakje? : ")).lower()
-            if hoorntje == "a":
-                deel = "hoorntje"
-                smaken()
-                bestel()
-            elif hoorntje == "b":
-                deel = "bakje"
-                smaken()
-                bestel()
-            else:
-                print("Sorry, dat snap ik niet")
-    elif bol >=4 and bol <=8:
-        deel = "bakje"
-        smaken()
-        bestel()
-    else:
-        print("Sorry, ik snap het niet...")
+        while bol >8:
+            print("Sorry, zulke grote bakken hebben we niet")
+            bol = int(input("Hoeveel bolletjes ijs wilt u? : "))
+        if bol >=1 and bol <=3:
+            while bak == 1:
+                hoorntje = str(input("Wilt u deze " + str(bol) + " bolletje(s) in A) een hoorntje of B) een bakje? : ")).lower()
+                if hoorntje == "a":
+                    hoorntjeAantal += 1
+                    deel = "hoorntje"
+                    smaken()
+                    bestel()
+                elif hoorntje == "b":
+                    bakjeAantal+= 1
+                    deel = "bakje"
+                    smaken()
+                    bestel()
+                else:
+                    print("Sorry, dat snap ik niet")
+        elif bol >=4 and bol <=8:
+            deel = "bakje"
+            smaken()
+            bestel()
+        else:
+            print("Sorry, ik snap het niet...")
+print("Welkom bij Papi Gelato. ")
+begin()
